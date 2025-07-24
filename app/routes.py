aa327d0f765e25a30d5068887f2ed5402d93458b
LYuree <118727153+LYuree@users.noreply.github.com>
2025-07-24 09:06:35 +0700

    Update routes.py
    Исправил ссылку redirect (подтверждение аккаунта).

--- app/routes.py
+++ app/routes.py
@@ -162,7 +162,7 @@ def get_user(activation_token: str, request: Request, db: Session = Depends(get_
     flag_modified(user, "active")
     db.commit()
     db.refresh(user)
-    redirect_url = 'http://localhost:3000/signin'
+    redirect_url = f"""{API_URL}/signin"""
     return RedirectResponse(redirect_url, status_code=status.HTTP_303_SEE_OTHER)
 
 # @router.get("/get-role")
