ced7cdd502ace93bb062a2b3fda085ddcdeb6052
LYuree <slymerrow@gmail.com>
2025-07-30 17:27:32 +0700

    Импорты UUID.

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@
 from sqlalchemy import JSON, Boolean, Column, Float, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
-# from sqlalchemy.dialects.postgresql import UUID
-from uuid import UUID
+from sqlalchemy.dialects.postgresql import UUID
+# from uuid import UUID
 
 
 
